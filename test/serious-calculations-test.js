20a117501a23a33eef63d25ea8bf4b2f87930c31
Marc Püls <mpuels@gmail.com>
2018-08-15 20:58:45 +0200

    Improve test message

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -5,10 +5,9 @@ var assert = require('assert'),
 vows.describe('serious-calculations').addBatch({
   'When performing serious calculations': {
     topic: seriousCalculations.performSeriousCalculations(4),
-    'result should be valid': function (result) {
+    'result should be valid!': function (result) {
       assert.isNumber(result);
       assert.equal(result, 8);
     }
   }
 }).export(module);
-
